57654d75ebbfa0b126b94277301ad634ffdd688f
Nephilus-notes <114515979+Nephilus-notes@users.noreply.github.com>
2023-10-30 10:08:13 -0500

    adjusted event name to LiveEvent, add api-service files

--- src/app/models/event.ts
+++ src/app/models/event.ts
@@ -1,4 +1,4 @@
-export interface Event {
+export interface LiveEvent {
     id: number;
     title: string;
     description: string;
---
+++ src/app/services/api-service.service.ts
@@ -0,0 +1,15 @@
+import { Injectable } from '@angular/core';
+import { Observable } from 'rxjs';
+
+import { LiveEvent } from '../models/event';
+import { Setlist } from '../models/setlist';
+import { Song } from '../models/song';
+import { Patron } from '../models/patron';
+
+@Injectable({
+  providedIn: 'root'
+})
+export class ApiServiceService {
+
+  constructor() { }
+}
