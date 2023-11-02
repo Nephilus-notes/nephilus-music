293ed4d0b35f88c41c55d8bf388953e98ce273a7
Nephilus-notes <114515979+Nephilus-notes@users.noreply.github.com>
2023-11-02 17:39:40 -0500

    sing song up and running, with routing for parameters

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -5,12 +5,20 @@ import { BioViewerComponent } from './views/bio-viewer/bio-viewer.component';
 import { EventViewerComponent } from './views/event-viewer/event-viewer.component';
 import { PressViewerComponent } from './views/press-viewer/press-viewer.component';
 import { AdminViewComponent } from './views/admin-view/admin-view.component';
+import { SingleSongComponent } from './components/single-song/single-song.component';
+import { SinglePatronComponent } from './components/single-patron/single-patron.component';
+import { SingleSetlistComponent } from './components/single-setlist/single-setlist.component';
+import { SingleEventComponent } from './components/single-event/single-event.component';
 
 const routes: Routes = [
   { path: '', component: IndexComponent },
   { path: 'bio', component: BioViewerComponent },
   { path: 'events', component: EventViewerComponent },
   { path: 'materials', component: PressViewerComponent},
+  { path: 'song/:id', component: SingleSongComponent},
+  { path: 'patron/:id', component: SinglePatronComponent},
+  { path: 'setlist/:id', component: SingleSetlistComponent},
+  { path: 'event/:id', component: SingleEventComponent},
   { path: 'admin', component: AdminViewComponent},
   { path: '**', component: IndexComponent}
 ];
--- src/app/components/single-song/single-song.component.ts
+++ src/app/components/single-song/single-song.component.ts
@@ -1,4 +1,7 @@
 import { Component } from '@angular/core';
+import { ActivatedRoute } from '@angular/router';
+import { Song } from 'src/app/models/song';
+import { ApiService } from 'src/app/services/api.service';
 
 @Component({
   selector: 'app-single-song',
@@ -6,5 +9,21 @@ import { Component } from '@angular/core';
   styleUrls: ['./single-song.component.css']
 })
 export class SingleSongComponent {
+  id!: number;
+song!: Song;
 
+  constructor(private route: ActivatedRoute, private apiService: ApiService) { }
+
+  public getSong(): void {
+    this.song = this.apiService.getOneSong(this.id)
+  }
+
+  ngOnInit(): void {
+    let id = this.route.snapshot.paramMap.get('id');
+    if (id) {
+      
+      this.id = +id;
+    }
+    this.getSong()
+  }
 }
