f06d94c6ac47dc88bccc6a598c9f6633c3c6e554
Nephilus-notes <114515979+Nephilus-notes@users.noreply.github.com>
2023-12-03 11:45:49 -0600

    made nav absolute and adjusted other things around it.

--- src/app/views/event-viewer/event-viewer.component.ts
+++ src/app/views/event-viewer/event-viewer.component.ts
@@ -6,28 +6,34 @@ import { LiveEvent } from 'src/app/models/liveEvent';
 @Component({
   selector: 'app-event-viewer',
   templateUrl: './event-viewer.component.html',
-  styleUrls: ['./event-viewer.component.css'],
+  styleUrls: ['./event-viewer.component.css']
 })
 export class EventViewerComponent {
-  constructor(private apiService: ApiService) {}
+
+  constructor( private apiService: ApiService) {}
 
   show_list: LiveEvent[] = [];
   showControl: number = 1;
 
+  
   public getAllEvents(): void {
-    let full_list = [...this.apiService.getAllEvents()];
+
+    let full_list = [...this.apiService.getAllEvents()]
     for (let show of full_list) {
-      let today = new Date();
-      console.log(show.start_date, today);
+      let today = new Date()
+      console.log(show.start_date, today)
       if (show.start_date > today) {
-        this.show_list.push(show);
-      } else {
-        console.log('show has not happened yet');
+        this.show_list.push(show)
+      }
+      else {
+        console.log('show has not happened yet')
       }
     }
-  }
+    }
 
-  ngOnInit(): void {
-    this.getAllEvents();
-  }
+
+
+    ngOnInit(): void {
+      this.getAllEvents()
+    }
 }
