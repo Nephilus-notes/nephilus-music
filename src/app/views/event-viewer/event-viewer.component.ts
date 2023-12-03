05606405628bc43c05419fc4d7df2bbebf7182cf
Nephilus-notes <114515979+Nephilus-notes@users.noreply.github.com>
2023-12-03 10:33:49 -0600

    fixed bug in event display viewer in event array

--- src/app/views/event-viewer/event-viewer.component.ts
+++ src/app/views/event-viewer/event-viewer.component.ts
@@ -6,32 +6,28 @@ import { LiveEvent } from 'src/app/models/liveEvent';
 @Component({
   selector: 'app-event-viewer',
   templateUrl: './event-viewer.component.html',
-  styleUrls: ['./event-viewer.component.css']
+  styleUrls: ['./event-viewer.component.css'],
 })
 export class EventViewerComponent {
+  constructor(private apiService: ApiService) {}
 
-  constructor( private apiService: ApiService) {}
-
-  show_list!: LiveEvent[]
+  show_list: LiveEvent[] = [];
   showControl: number = 1;
 
-  
   public getAllEvents(): void {
-
-    let full_list = [...this.apiService.getAllEvents()]
+    let full_list = [...this.apiService.getAllEvents()];
     for (let show of full_list) {
-      if (show.start_date < new Date) {
-        full_list.splice(full_list.indexOf(show), 1)
+      let today = new Date();
+      console.log(show.start_date, today);
+      if (show.start_date > today) {
+        this.show_list.push(show);
+      } else {
+        console.log('show has not happened yet');
       }
     }
+  }
 
-    this.show_list = full_list
-    // console.warn(this.show_list)
-    }
-
-
-
-    ngOnInit(): void {
-      this.getAllEvents()
-    }
+  ngOnInit(): void {
+    this.getAllEvents();
+  }
 }
