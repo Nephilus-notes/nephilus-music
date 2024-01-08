73d12996c668b0aad6aaa6e362907fff2169726f
Nephilus-notes <114515979+Nephilus-notes@users.noreply.github.com>
2024-01-08 14:22:26 -0600

    rough geolocation work

--- src/app/views/index/index.component.ts
+++ src/app/views/index/index.component.ts
@@ -1,4 +1,5 @@
 import { Component } from '@angular/core';
+import { LoggerService } from 'src/app/services/logger.service';
 
 @Component({
   selector: 'app-index',
@@ -6,6 +7,11 @@ import { Component } from '@angular/core';
   styleUrls: ['./index.component.css']
 })
 export class IndexComponent {
+  constructor(private logger:LoggerService) {}
+  ngOnInit(): void {
+    // this.logger.logLocation();
+    this.logger.apiLocationLog();
+  }
 title= "Charles McCall - Dev and Creative"
 musicUrlList: Array<string> = ["https://drive.google.com/file/d/1wD7Beul2z8LMgrgPnG33Ik0kuIuNKF1A/preview", "https://drive.google.com/file/d/1U9GbAlxaBik5ZilaQAfme1ARyUfLmU5e/preview"]
 musicNameList: Array<string> = ["Everything But Desire", "Trouble You Can Borrow"] 
