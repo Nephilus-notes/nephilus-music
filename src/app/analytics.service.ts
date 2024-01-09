cc929c0580ff864fd08ccadd2c2f3484dd8e84c0
Nephilus-notes <114515979+Nephilus-notes@users.noreply.github.com>
2024-01-09 15:32:27 -0600

    working time on page analytics. scaffolded service

---
+++ src/app/analytics.service.ts
@@ -0,0 +1,10 @@
+import { Injectable } from '@angular/core';
+
+@Injectable({
+  providedIn: 'root'
+})
+export class AnalyticsService {
+
+  constructor() { }
+  pagesViewed: any[] = [];
+}
--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,10 +1,39 @@
 import { Component } from '@angular/core';
+import { Router } from '@angular/router';
 
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
-  styleUrls: ['./app.component.css']
+  styleUrls: ['./app.component.css'],
 })
 export class AppComponent {
   title = 'nephilus-music';
+  lastIn: any = '';
+  currentPage: any = '';
+  timeSpentOnPages: any[] = [];
+
+  constructor(private router: Router) {
+    this.router.events.subscribe((val: any) => {
+      let checking = 0;
+      if (!this.currentPage) {
+        this.currentPage = val.url;
+        this.lastIn = Date.now();
+        checking ++;
+        console.log(checking)
+      }
+      if (this.currentPage !== val.url) {
+        const timeSpent = Date.now() - this.lastIn;
+        console.log(this.currentPage + ' timespent ' + timeSpent);
+        const pageInfo = {
+          pageUrl: this.currentPage,
+          timeSpent,
+        };
+        checking ++;
+        console.log(checking)
+        this.timeSpentOnPages.push(pageInfo);
+        this.lastIn = Date.now();
+        this.currentPage = val.url;
+      }
+    });
+  }
 }
