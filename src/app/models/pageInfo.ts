2378d9ab1680d3f9877c156d3d59d081a859eed7
Nephilus-notes <114515979+Nephilus-notes@users.noreply.github.com>
2024-01-09 15:35:16 -0600

    pageinfo interface

---
+++ src/app/models/pageInfo.ts
@@ -0,0 +1,11 @@
+export interface pageAnalytics {
+    pageUrl: string;
+    pageName: string;
+    views: number;
+    uniqueViews: number;
+    timeOnPage: number;
+    priorPages: Array<string>;
+    nextPages: Array<string>;
+    uniqueIpAddresses: Array<string>;
+
+}
