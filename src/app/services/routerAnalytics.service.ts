14ea462822c8fafe32b6113e09c8f917bb1f2a11
Nephilus-notes <114515979+Nephilus-notes@users.noreply.github.com>
2024-01-09 17:38:02 -0600

    working on adding history to router analytics.

--- src/app/services/routerAnalytics.service.ts
+++ src/app/services/routerAnalytics.service.ts
@@ -10,9 +10,12 @@ export class AnalyticsService {
 
   lastIn: number = 0;
   currentPage!: pageAnalytics;
+  previousUrl!: string;
+  currentUrl!: string;
 
   setCurrentPage() {
     this.router.events.subscribe((event: any) => {
+      // console.log(event)
       // check if event is a scroll event (type 15) the final event in the Angular Router chain
       if (event.type === 15) {
         // create current page if it does not exis
@@ -30,18 +33,30 @@ export class AnalyticsService {
             nextPages: [],
             uniqueIpAddresses: [],
           };
+          this.currentUrl = event.routerEvent.url;
         } else if (this.currentPage) {
           // if current page exists, add time spent on page to timeOnPage and add it to the cache
           this.currentPage.timeOnPage += Number(Date.now() - this.lastIn);
 
+          // combine with conditional below?
+          // if (this.currentPage.pageUrl !== event.routerEvent.url) {
+          //   this.previousUrl = this.currentPage.pageUrl;
+          // }
+
+
           // check if page is in cache
           if (this.pagesViewed[event.routerEvent.url] !== undefined) {
             // because page is in cache, increment views and set to currentPage
+
             this.pagesViewed[event.routerEvent.url].views++;
             this.currentPage = this.pagesViewed[event.routerEvent.url];
+            this.previousUrl = this.currentUrl;
+            this.currentUrl = event.routerEvent.url;
           }
         }
-
+        // if(this.previousUrl){
+          this.currentPage.priorPages.push(this.previousUrl);
+        // }
         this.addPageToCache();
         console.log(this.pagesViewed);
         this.lastIn = Date.now();
