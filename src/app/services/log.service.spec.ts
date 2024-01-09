5bf48b78c22442067dbab36422fb5ab4432f6753
Nephilus-notes <114515979+Nephilus-notes@users.noreply.github.com>
2024-01-09 11:36:58 -0600

    added more environment variables

--- src/app/services/log.service.spec.ts
+++ src/app/services/log.service.spec.ts
@@ -1,13 +1,13 @@
 import { TestBed } from '@angular/core/testing';
 
-import { LoggerService } from './logger.service';
+import { LogService } from './log.service';
 
-describe('LoggerService', () => {
-  let service: LoggerService;
+describe('LogService', () => {
+  let service: LogService;
 
   beforeEach(() => {
     TestBed.configureTestingModule({});
-    service = TestBed.inject(LoggerService);
+    service = TestBed.inject(LogService);
   });
 
   it('should be created', () => {
