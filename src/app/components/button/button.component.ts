85679038ee85be09df7938c5d44e4f4ff140d0d7
Nephilus-notes <114515979+Nephilus-notes@users.noreply.github.com>
2024-01-11 13:07:43 -0600

    button id functional and variable

--- src/app/components/button/button.component.ts
+++ src/app/components/button/button.component.ts
@@ -7,9 +7,17 @@ import { ButtonInfo } from 'src/app/models/buttonInfo';
   styleUrls: ['./button.component.css']
 })
 export class ButtonComponent {
-  @Input() URl!: string;
-  @Input() text!: string;
+
   @Input() buttonInfo!: ButtonInfo;
+
+  @Input() id!:string;
+
+  ngOnChanges(): void {
+    if(!this.id) {
+      this.id = this.buttonInfo.id;
+    }
+  }
+
 
   // buttonInfo: ButtonInfo = {
   //   id: 'testButton',
