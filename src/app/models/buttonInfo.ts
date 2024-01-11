f93d36816493ec236ea501c31a263c21baf11fc2
Nephilus-notes <114515979+Nephilus-notes@users.noreply.github.com>
2024-01-11 12:13:36 -0600

    added button info interface

---
+++ src/app/models/buttonInfo.ts
@@ -0,0 +1,10 @@
+export interface ButtonInfo {
+    id: string;
+    type: string;
+    text: string;
+    url?: string;
+    function: Function;
+    classes: string;
+    title: string;
+    ariaLabel: string;
+}
